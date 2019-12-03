de179fd94e58d4cc1429dec2a4aa7f96768da2a8
josescuderoh <josescuderoh@icloud.com>
2019-12-03 12:45:14 -0500

    Added break_string function

--- readme.md
+++ readme.md
@@ -5,4 +5,4 @@ This repo contains a script with a series useful functions for data processing i
 * `fix_names()`: This function takes a list of column names and standardizes the names
  for Python analysis returning a list of fixed names.
 
- * (...)
+ * `break_string`: Take one string and break it into a list of elements given the spaces inside the string.
--- utils.py
+++ utils.py
@@ -13,6 +13,28 @@ def fix_names(names):
 
   return fixed_names
 
+def break_string(string):
+    """
+    Take one string and break it into a list of elements given the spaces inside the string.
+    Example:
+
+    >>> break_string('RA RA FZFG BR')
+    {'RA': 2, 'FZFG': 1, 'BR': 1}
+    """
+
+    import re
+    from collections import defaultdict
+
+    if string.strip() != '':
+        sym_count = defaultdict(int)
+        for sym in re.split('\s+', string.strip()):
+            sym_count[sym] += 1
+    else:
+        sym_count = {}
+
+    return dict(sym_count)
+
 if __name__ == '__main__':
   import doctest
   doctest.run_docstring_examples(fix_names, globals())
+  doctest.run_docstring_examples(break_string, globals())
