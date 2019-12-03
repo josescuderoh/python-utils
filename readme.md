1b06e0ee0990f316731c52cadacfca57b3060f4b
josescuderoh <josescuderoh@icloud.com>
2019-12-03 12:49:49 -0500

    Added get_previous_quantity function

--- readme.md
+++ readme.md
@@ -1,8 +1,13 @@
 # Python Utils
 
-This repo contains a script with a series useful functions for data processing in Python:
+This repo (under construction) contains a script with a series useful functions for data processing in Python tested using doctest and with examples:
 
 * `fix_names()`: This function takes a list of column names and standardizes the names
  for Python analysis returning a list of fixed names.
 
- * `break_string`: Take one string and break it into a list of elements given the spaces inside the string.
+ * `break_string()`: Take one string and break it into a list of elements given the spaces inside the string.
+
+ * `get_previous_quantity()`: Takes a dataframe with a datetime column and a numerical column and appends a new column with the
+   quantity for the previous day. If there were no quantities the day before, it returns zero.
+
+* (...)
--- utils.py
+++ utils.py
@@ -34,7 +34,45 @@ def break_string(string):
 
     return dict(sym_count)
 
+def get_previous_quantity(df, q='units', date='date'):
+    """Takes a dataframe with a datetime column and a numerical column and appends a new column with the
+    quantity for the previous day. If there were no quantities the day before, it returns zero.
+
+    >>> from datetime import datetime, timedelta
+    >>> import pandas as pd
+
+    >>> sales = {'date': [datetime.today() - timedelta(days=4),\
+    datetime.today() - timedelta(days=2),\
+    datetime.today() - timedelta(days=1),\
+    datetime.today()], \
+    'units': [5,4,3,2]}
+
+    >>> sales_df = pd.DataFrame(sales)
+
+    >>> get_previous_quantity(sales_df)['previous_units'].to_list()
+    [0.0, 0.0, 4.0, 3.0]
+
+    """
+    from datetime import timedelta
+
+    df = df.sort_values(by=date, ascending=True)
+
+    # Calculate previous day's units
+    df['previous_units'] = df[q].shift(1)
+    df.loc[df['previous_units'].isna(), 'previous_units'] = 0
+
+    # Flag if the difference between current and previous is 1 day
+    df['has_prev_day'] = df[date].diff() == timedelta(1)
+
+    # Set to zero if it's not
+    df.loc[df['has_prev_day'] == False, 'previous_units'] = 0
+
+    df.drop(columns=['has_prev_day'], inplace=True)
+
+    return df
+
 if __name__ == '__main__':
   import doctest
   doctest.run_docstring_examples(fix_names, globals())
   doctest.run_docstring_examples(break_string, globals())
+  doctest.run_docstring_examples(get_previous_quantity, globals())
